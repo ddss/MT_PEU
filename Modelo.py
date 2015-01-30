b22016619ed8dab2c738ecd64ef97e2f930e5f04
ddss <ddssantana89@gmail.com>
2015-01-30 18:43:34 -0300

    Análise de Resíduos
    Merge com o commit cd9c940800bdae39d0963b51024a6997513800d9 de indicadores.
    
    O PEU realiza a análise de resíduos através de testes estatísticos incluídos no Atributo estatisticas (Grandeza residuo): (i) com 2 testes para a média (ttest e ttest_ind), (ii) 4 testes de normalidade (anderson, shapiro, kstest, normaltest). Além dos testes estatísticos, foram incluidos gráficos para avaliar a autocorrelação dos resíduos (autocorr), normalidade (probplot), pontos espúrios (boxplot), distribuição (histrograma) e aleatoriade (resíduos em função do ponto experimental).
    
    Classe Grandeza: possui dois novos métodos: _testesEstatisticos e Graficos
    
    Classe Estimacao: o método analiseResiduos está funcional (apenas para os resíduos de y)
    
    Correções gerais:
    Retirado o use('Agg') do PSO.

--- Modelo.py
+++ Modelo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from threading import Thread
-from numpy import concatenate
+from numpy import concatenate, exp
 
 class Modelo(Thread):
     result = 0
@@ -18,24 +18,21 @@ class Modelo(Thread):
 
         alpha = self.param[0]
         beta  = self.param[1]
-        #gama  = self.param[2]
-        #eta   = self.param[3]
-        #y1 = alpha*(x**beta)
-        y1 = alpha*x/(1+beta*x)
-        #y2 = gama*x2  + eta
-
-
-#        tempo = self.x[:,0:1]
-#        T     = self.x[:,1:2]
-#        
-#        ko = self.param[0]
-#        E  = self.param[1]
-#        #gama  = self.param[2]
-#        #eta   = self.param[3]
-#        #y1 = exp(-(ko*10**17)*tempo*exp(-E/T))
-#        #y1 = exp(-tempo*exp(ko-E/T)) 
-#        y1 = exp(-ko*tempo*exp(-E*(1/T-1./630.)))
+        
+        y1 = alpha*(x**beta)
         #y1 = alpha*x/(1+beta*x)
 
+
+        #tempo = self.x[:,0:1]
+        #T     = self.x[:,1:2]
+        
+        #ko = self.param[0]
+        #E  = self.param[1]
+        #gama  = self.param[2]
+        #eta   = self.param[3]
+        #y1 = exp(-(ko*10**17)*tempo*exp(-E/T))
+        #y1 = exp(-tempo*exp(ko-E/T)) 
+        # y1 = exp(-ko*tempo*exp(-E*(1/T-1./630.)))
+        #y1 = alpha*x/(1+beta*x)
 
         self.result = y1
