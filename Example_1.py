112d882308a1c76b542ee8136718c4a2fafd8031
ddss <ddssantana89@gmail.com>
2020-05-01 11:45:24 -0300

    reversão parcia do commit 156876be015b43e19c1f7f15921148ad80fbe6e0
    - mantidas alterações nos Exemplos 1 e 3 inclusas no commit 3a841302e2a4557a21aa2720293f1568b25b5c48

--- Example_1.py
+++ Example_1.py
@@ -1,6 +1,8 @@
+#%% Packages importing
 from MT_PEU import EstimacaoNaoLinear
 from numpy import exp
 
+#%% Model definition
 def Modelo(param,x,*args):
 
     ko, E = param[0], param[1]
@@ -8,49 +10,55 @@ def Modelo(param,x,*args):
 
     return exp(-(ko*10**17)*tempo*exp(-E/T))
 
-Estime = EstimacaoNaoLinear(Modelo, simbolos_x=['t','Tao'], simbolos_y=['y'], simbolos_param=['ko','E'], Folder = 'Exemplo1')
+#%% Starting the MT_PEU main object
+Estime = EstimacaoNaoLinear(Modelo, simbolos_x=['t','Tao'], simbolos_y=['y'], simbolos_param=['ko','E'], Folder='Exemplo1')
 
+#%% Defining observed data
+# Output data
 y = [0.9,0.949,0.886,0.785,0.791,0.890,0.787,0.877,0.938,
 0.782,0.827,0.696,0.582,0.795,0.800,0.790,0.883,0.712,0.576,0.715,0.673,
 0.802,0.802,0.804,0.794,0.804,0.799,0.764,0.688,0.717,0.802,0.695,0.808,
 0.655,0.309,0.689,0.437,0.425,0.638,.659,0.449]
-
+# output uncertainty
+uy = [1]*41
+# input 1
 tempo = [120.0,60.0,60.0,120.0,120.0,60.0,60.0,30.0,15.0,60.0,
 45.1,90.0,150.0,60.0,60.0,60.0,30.0,90.0,150.0,90.4,120.0,
 60.0,60.0,60.0,60.0,60.0,60.0,30.0,45.1,30.0,30.0,45.0,15.0,30.0,90.0,25.0,
 60.1,60.0,30.0,30.0,60.0]
-
+# input 1 uncertainty
+uxtempo = [1]*41
+# input 2
 temperatura = [600.0,600.0,612.0,612.0,612.0,612.0,620.0,620.0,620.0,
 620.0,620.0,620.0,620.0,620.0,620.0,620.0,620.0,620.0,620.0,620.0,620.0,
 620.0,620.0,620.0,620.0,620.0,620.0,631.0,631.0,631.0,631.0,631.0,639.0,639.0,
 639.0,639.0,639.0,639.0,639.0,639.0,639.0]
+# input 2 uncertainty
+uxtemperatura = [1]*41
 
-constante = 10**17
-
-uy = [1]*41; uxtempo = [1]*41; uxtemperatura = [1]*41
-
+#%% Setting the observed data set
+# inputs
 Estime.setDados(0,(tempo,uxtempo),(temperatura,uxtemperatura))
+# outputs
 Estime.setDados(1,(y,uy))
 
+# Defining the previous data set to be used to parameter estimation
 Estime.setConjunto(tipo='estimacao')
-#Estime.graficos()
 
+#%% Optimization - estimating the parameters
 Estime.optimize(initial_estimative=[0.5,25000], algoritmo='ipopt')
 
+#%% Evaluating the parameters uncertainty and coverage region
 Estime.incertezaParametros(metodoIncerteza='Geral')
+
+#%% Evaluating model predictions
 Estime.predicao()
+
+#%% Evaluating residuals and quality index
 Estime.analiseResiduos()
+
+#%% Plotting the main results
 Estime.graficos()
 
-etapas = ['grandezas-entrada', 'predicao', 'grandezas-calculadas', 'analiseResiduos', 'regiaoAbrangencia']
-
-
-
-
-
-
-
-
-
-
-
+#%% Reference of this case study
+# SCHWAAB, M.M.;PINTO, J.C. Análise de Dados Experimentais I: Fundamentos da Estátistica e Estimação de Parâmetros. Rio de Janeiro: e-papers, 2007.
