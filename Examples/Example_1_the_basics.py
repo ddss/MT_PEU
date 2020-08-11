e76df7b15d01d1762eb6826a0cc5693031fdd9e2
Matheus2301 <matheusreissantiago@gmail.com>
2020-08-10 23:12:18 -0300

    Adequações entre a versão '.py' e '.ipynb'

--- Examples/Example_1_the_basics.py
+++ Examples/Example_1_the_basics.py
@@ -3,6 +3,7 @@ from MT_PEU import EstimacaoNaoLinear
 from numpy import exp
 
 #%% Model definition
+# def Model: The def model specifies the equations with their respective parameters.
 def Model(param,x,*args):
 
     ko, E = param[0], param[1]
@@ -11,6 +12,11 @@ def Model(param,x,*args):
     return exp(-(ko*10**17)*time*exp(-E/T))
 
 #%% Starting the MT_PEU main object
+# Model: Pass the model defined in def Model;
+# symbols_x: Symbols for quantity x;
+# symbols_y: Symbols for quantity y;
+# symbols_param:Symbols for the parameters to be estimated;
+# Folder: Defines the name of the folder where the results will be saved.
 Estime = EstimacaoNaoLinear(Model, symbols_x=['t','Tao'], symbols_y=['y'], symbols_param=['ko','E'], Folder='KineticModelEX1')
 
 #%% Defining observed data
@@ -43,10 +49,10 @@ Estime.setDados(0,(time,uxtime),(temperature,uxtemperature))
 Estime.setDados(1,(y,uy))
 
 # Defining the previous data set to be used to parameter estimation
-Estime.setConjunto(dataType='estimacao')
+Estime.setConjunto()
 
 #%% Optimization - estimating the parameters
-Estime.optimize(initial_estimative=[0.5,25000], algorithm='ipopt')
+Estime.optimize(initial_estimative=[0.5,25000])
 
 #%% Evaluating the parameters uncertainty and coverage region
 Estime.parametersUncertainty()
@@ -59,7 +65,6 @@ Estime.residualAnalysis()
 
 #%% Plotting the main results
 Estime.plots()
-
 
 #%% Reference of this case study
 # SCHWAAB, M.M.;PINTO, J.C. Análise de Dados Experimentais I: Fundamentos da Estátistica e Estimação de Parâmetros. Rio de Janeiro: e-papers, 2007.
